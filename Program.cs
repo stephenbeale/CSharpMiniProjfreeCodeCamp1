a30d2a8b217bdeb4570ddabf90a8bde031529233
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-08 10:51:50 +0100

    Negative numbers

--- Program.cs
+++ Program.cs
@@ -11,10 +11,26 @@ namespace CSharpMiniProjfreeCodeCamp1
         static void Main(string[] args)
         {
             int age = 23;
-            Console.WriteLine(age);
+            Console.WriteLine($"age: {age}");
+
+            Console.WriteLine($"int.MaxValue {int.MaxValue}");
+            Console.WriteLine($"int.MinValue {int.MinValue}");
 
             //L enforces long, rather than int32
             long bigNumber = 90000000L;
+
+            Console.WriteLine($"long.MinValue {long.MinValue}");
+            Console.WriteLine($"long.MaxValue {long.MaxValue}");
+            Console.WriteLine($"bigNumber {bigNumber}");
+
+            long bigNumberNegative = -90000000L;
+            Console.WriteLine($"bigNumberNegative {bigNumberNegative}");
+
+            //D at end enforces double, if unsure
+            double negative = -55.2D;
+
+            Console.WriteLine($"negative {negative}");
+
             Console.ReadLine();
         }
     }
