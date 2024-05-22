67e03553a6292aa8b631177a7942a312bc3cd0b7
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-22 17:44:20 +0100

    WIP - end of day

--- Program_ remainders.cs
+++ Program_ remainders.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
 
-            //Continue from: https://youtu.be/GlGpSQsS8uc?si=nEICiUGY7sZdaMTI&t=1884
+//            //Continue from: https://youtu.be/GlGpSQsS8uc?si=nEICiUGY7sZdaMTI&t=1884
 
-            int age = 23;
-            age++;
+//            int age = 23;
+//            age++;
 
 
-            int firstNum = 10;
-            int secondNum = 3;
+//            int firstNum = 10;
+//            int secondNum = 3;
 
-            Console.WriteLine($"Direct division: {firstNum / secondNum}");
+//            Console.WriteLine($"Direct division: {firstNum / secondNum}");
 
-            Console.WriteLine($"Remainder: { firstNum % secondNum}");
+//            Console.WriteLine($"Remainder: { firstNum % secondNum}");
 
-            Console.WriteLine($"10 / 3 is even? { evenOdd(10, 3)}");
+//            Console.WriteLine($"10 / 3 is even? { evenOdd(10, 3)}");
 
-            Console.WriteLine($"9 / 3 is even? {evenOdd(9, 3)}");
+//            Console.WriteLine($"9 / 3 is even? {evenOdd(9, 3)}");
 
-            Console.ReadLine();
-        }
+//            Console.ReadLine();
+//        }
 
-        //Adding static means it can be called here - otherwise it's an 'instance' method, and can't
-        public static bool evenOdd(int num, int divisor)
-        {
-            return num % divisor == 0;
-        }
-    }
-}
+//        //Adding static means it can be called here - otherwise it's an 'instance' method, and can't
+//        public static bool evenOdd(int num, int divisor)
+//        {
+//            return num % divisor == 0;
+//        }
+//    }
+//}
---
+++ Program_ vars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Continue from: https://youtu.be/GlGpSQsS8uc?si=sedUOcFgDGRhaAEa&t=2095
+
+            //Var must be initialised
+            var age = 23;
+
+            Console.ReadLine();
+        }
+    }
+}
