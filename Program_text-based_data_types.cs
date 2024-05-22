3813c5694e4be52958148fe1b8850ac9e84c542d
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-22 12:36:08 +0100

    Basic strings

---
+++ Program_text-based_data_types.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Continue from: https://youtu.be/YrtFtdTTfv0?si=d-oKGjndWmYv_p3g&t=1234
+            string name = "Aba";
+            //Char with single quotation
+            char letter = 'a';
+
+            Console.WriteLine($"Your name is {name}");
+            Console.WriteLine($"The letter is {letter}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
