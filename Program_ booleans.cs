86ff45f5b79890526388dc66004ae26b3f42b0b0
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-22 12:59:28 +0100

    Printing some bools

---
+++ Program_ booleans.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Continue from: https://youtu.be/YrtFtdTTfv0?si=5SFqXzL43tkalVEE&t=1469
+
+            //Use booleans for making decisions
+            bool value = true;
+            Console.WriteLine(value);
+            bool isMale = true;
+            Console.WriteLine(isMale);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
--- Program_text-based_data_types.cs
+++ Program_text-based_data_types.cs
@@ -1,60 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
 
-            //Continue from: https://youtu.be/YrtFtdTTfv0?si=usRziHVM6i2itCR4&t=1350
-            string name = "Aba";
-            //Char with single quotation
-            char letter = 'a';
+//            //Continue from: https://youtu.be/YrtFtdTTfv0?si=usRziHVM6i2itCR4&t=1350
+//            string name = "Aba";
+//            //Char with single quotation
+//            char letter = 'a';
 
-            Console.WriteLine($"Your name is {name}");
-            Console.WriteLine($"The letter is {letter}");
+//            Console.WriteLine($"Your name is {name}");
+//            Console.WriteLine($"The letter is {letter}");
 
-            string textAge = "-23";
-            Console.WriteLine($"Text age as string: {textAge}");
-            //Converts back to int
-            int age = Convert.ToInt32(textAge);
-            Console.WriteLine($"Text age as converted int: {textAge}");
+//            string textAge = "-23";
+//            Console.WriteLine($"Text age as string: {textAge}");
+//            //Converts back to int
+//            int age = Convert.ToInt32(textAge);
+//            Console.WriteLine($"Text age as converted int: {textAge}");
 
-            string bigNumber = "-90000000";
-            Console.WriteLine($"Big# as string: {bigNumber}");
-            //L enforces long, rather than int32
-            long big = Convert.ToInt64(bigNumber);
-            //long bigNumber = 90000000L;
-            Console.WriteLine($"Big# as converted long: {bigNumber}");
+//            string bigNumber = "-90000000";
+//            Console.WriteLine($"Big# as string: {bigNumber}");
+//            //L enforces long, rather than int32
+//            long big = Convert.ToInt64(bigNumber);
+//            //long bigNumber = 90000000L;
+//            Console.WriteLine($"Big# as converted long: {bigNumber}");
 
 
-            string textNegative = "-55.2";
-            Console.WriteLine($"Text negative double as string: {textNegative}");
-            double negative = Convert.ToDouble(textNegative);
-            Console.WriteLine($"Text negative as converted double: {negative}");
+//            string textNegative = "-55.2";
+//            Console.WriteLine($"Text negative double as string: {textNegative}");
+//            double negative = Convert.ToDouble(textNegative);
+//            Console.WriteLine($"Text negative as converted double: {negative}");
 
-            Console.WriteLine($"negative {negative}");
+//            Console.WriteLine($"negative {negative}");
 
-            //F at end to register number with decimal as a float, not a double
-            string floaty = "5.000001";
-            Console.WriteLine(floaty);
-            //Single because a float is defined as a single - it's a struct in the System.Single class
-            float floatyMcFloatFace = Convert.ToSingle(floaty);
-            Console.WriteLine(floaty);
+//            //F at end to register number with decimal as a float, not a double
+//            string floaty = "5.000001";
+//            Console.WriteLine(floaty);
+//            //Single because a float is defined as a single - it's a struct in the System.Single class
+//            float floatyMcFloatFace = Convert.ToSingle(floaty);
+//            Console.WriteLine(floaty);
 
-            string moneyString = "14.99";
-            Console.WriteLine(moneyString);
-            decimal money = Convert.ToDecimal(moneyString);
-            Console.WriteLine(money);
-            //M at end to override double
+//            string moneyString = "14.99";
+//            Console.WriteLine(moneyString);
+//            decimal money = Convert.ToDecimal(moneyString);
+//            Console.WriteLine(money);
+//            //M at end to override double
 
-            Console.ReadLine();
+//            Console.ReadLine();
 
-        }
-    }
-}
+//        }
+//    }
+//}
