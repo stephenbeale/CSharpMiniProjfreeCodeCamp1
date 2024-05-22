fa6470a837678ea09d726adb7c72248f2e149052
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-22 13:32:59 +0100

    Even / odd and remainder function

--- Program_ booleans.cs
+++ Program_ booleans.cs
@@ -1,61 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
 
-            //Continue from: https://youtu.be/GlGpSQsS8uc?si=WOjoNTeclBupEypC&t=1541
+//            //Continue from: https://youtu.be/GlGpSQsS8uc?si=fcq-8cunUvCF4AcI&t=1870
 
-            int age = 23;
-            age++;
-            Console.WriteLine(age);
-            //All do the same, but below can do calculations, too
-            age = age + 1;
-            age += 1;
-            age--;
-            Console.WriteLine(age);
+//            int age = 23;
+//            age++;
+//            Console.WriteLine(age);
+//            //All do the same, but below can do calculations, too
+//            age = age + 1;
+//            age += 1;
+//            age--;
+//            Console.WriteLine(age);
             
-            //Mult by 3
-            age *= 3;
-            Console.WriteLine(age);
+//            //Mult by 3
+//            age *= 3;
+//            Console.WriteLine(age);
             
-            //Divides by 10
-            age /= 10;
-            Console.WriteLine(age);
-            double doubleAge = Convert.ToDouble(age);
+//            //Divides by 10
+//            age /= 10;
+//            Console.WriteLine(age);
+//            double doubleAge = Convert.ToDouble(age);
 
-            doubleAge /= 2.4;
-            Console.WriteLine(doubleAge);
+//            doubleAge /= 2.4;
+//            Console.WriteLine(doubleAge);
 
-            string name = "Aba";
-            name += " is programming";
+//            string name = "Aba";
+//            name += " is programming";
 
-            Console.WriteLine(name);
+//            Console.WriteLine(name);
 
-            //Can't add a char - adds them as unicode characrers
-            char ch = 'a';
-            ch += 'b';
-            Console.WriteLine(ch);
+//            //Can't add a char - adds them as unicode characrers
+//            char ch = 'a';
+//            ch += 'b';
+//            Console.WriteLine(ch);
 
-            int i = 0;
-            i++;
-            Console.WriteLine(i);
-            Console.WriteLine(i++);
-            //put it before for it to increment THEN compile
-            Console.WriteLine(++i);
-            Console.WriteLine(i++);
-            Console.WriteLine(i);
+//            int i = 0;
+//            i++;
+//            Console.WriteLine(i);
+//            Console.WriteLine(i++);
+//            //put it before for it to increment THEN compile
+//            Console.WriteLine(++i);
+//            Console.WriteLine(i++);
+//            Console.WriteLine(i);
 
 
-            Console.ReadLine();
+//            Console.ReadLine();
 
-        }
-    }
-}
+//        }
+//    }
+//}
