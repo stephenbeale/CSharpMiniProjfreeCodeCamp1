a0825621e7d0ea47b57a4824045b45766626d2c8
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-24 14:11:56 +0100

    Console.Write vs WriteLine = same line vs next line.

--- Program_ console_input_output.cs
+++ Program_ console_input_output.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Schema;
@@ -14,10 +15,13 @@ namespace CSharpMiniProjfreeCodeCamp1
         {
             // https://youtu.be/GlGpSQsS8uc?si=51IVeikxAXQXzvbp&t=2803
 
-            Console.WriteLine("Enter your name:");
+            Console.Write("Enter your name:");
             string name = Console.ReadLine();
             Console.WriteLine($"Inputted name: {name}");
 
+
+            Console.Write("Enter your age:");
+            string age = Console.ReadLine();
             Console.ReadLine();
         }
     }
