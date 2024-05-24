7e4a54d131c5060373d3d1001571a6303c842d88
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-24 14:03:20 +0100

    Reassign any modified variables in calculations

--- Program_ const.cs
+++ Program_ const.cs
@@ -1,33 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics.Eventing.Reader;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml.Schema;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=h0Jjai45pv6P6OW6&t=2348
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=h0Jjai45pv6P6OW6&t=2348
 
-            const double vat = 20;
-            const double percentVat = vat / 100D;
-            const string version = "v1.0";
+//            const double vat = 20;
+//            const double percentVat = vat / 100D;
+//            const string version = "v1.0";
 
-            int balance = 1000;
+//            int balance = 1000;
 
-            Console.WriteLine($"VAT is {vat}%");
-            Console.WriteLine(balance * (vat / 100D));
-            Console.WriteLine(balance * percentVat);            
+//            Console.WriteLine($"VAT is {vat}%");
+//            Console.WriteLine(balance * (vat / 100D));
+//            Console.WriteLine(balance * percentVat);            
 
 
             
 
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.ReadLine();
+//        }
+//    }
+//}
