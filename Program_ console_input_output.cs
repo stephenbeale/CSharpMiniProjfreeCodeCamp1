a094e86c18aee91496a42e4b005aecd2ae467a38
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-24 15:07:39 +0100

    Multiplication task - continue here https://youtu.be/GlGpSQsS8uc?si=7QJQU2NYWoqyxsM7&t=3862

--- Program_ console_input_output.cs
+++ Program_ console_input_output.cs
@@ -13,38 +13,45 @@ namespace CSharpMiniProjfreeCodeCamp1
     {
         static void Main(string[] args)
         {
-            // https://youtu.be/GlGpSQsS8uc?si=OX43L1vYTGaORgCO&t=3298
+            // https://youtu.be/GlGpSQsS8uc?si=7QJQU2NYWoqyxsM7&t=3862
 
-            Console.Write("Enter your name:");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Inputted name: {name}");
+            //Console.Write("Enter your name:");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Inputted name: {name}");
 
-            Console.Write("Enter your age:");
-            //Converts directly from input
-            int agey = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter your age:");
+            ////Converts directly from input
+            //int agey = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Your age is {agey}");
+            //Console.WriteLine($"Your age is {agey}");
 
-            if (agey >= 18 && agey < 25)
-            {
-                Console.WriteLine("You are allowed in because you are 18 or older");
-            }
-            else if(agey >= 25 && agey < 40)
-            {
-                Console.WriteLine("25 or older");
-            }
-            else if (agey >= 40 && agey <= 75)
-            {
-                Console.WriteLine("40 - 75 years");
-            }
-            if(agey < 0 || agey > 115)
-            {
-                Console.WriteLine("Invalid age");
-            }
-            else
-            {
-                Console.WriteLine("Valid age");
-            };
+            //if (agey >= 18 && agey < 25)
+            //{
+            //    Console.WriteLine("You are allowed in because you are 18 or older");
+            //}
+            //else if(agey >= 25 && agey < 40)
+            //{
+            //    Console.WriteLine("25 or older");
+            //}
+            //else if (agey >= 40 && agey <= 75)
+            //{
+            //    Console.WriteLine("40 - 75 years");
+            //}
+            //if(agey < 0 || agey > 115)
+            //{
+            //    Console.WriteLine("Invalid age");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Valid age");
+            //};
+
+            Console.Write("Enter first number: ");
+            int numberA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int numberB = Convert.ToInt32(Console.ReadLine());
+
 
             Console.ReadLine();
         }
