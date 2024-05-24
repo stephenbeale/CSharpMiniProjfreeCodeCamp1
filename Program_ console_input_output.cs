9af52de42f70e85b998d0b33e7448ba560bf417d
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-24 14:06:40 +0100

    Console.readline actually returns a string

---
+++ Program_ console_input_output.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=51IVeikxAXQXzvbp&t=2803
+
+            Console.WriteLine("Enter your name:");
+
+            Console.ReadLine();
+        }
+    }
+}
--- Program_ storing_user_data.cs
+++ Program_ storing_user_data.cs
@@ -1,48 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics.Eventing.Reader;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml.Schema;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=vKBbpl_Qgo5WGdHk&t=2578
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=vKBbpl_Qgo5WGdHk&t=2578
 
-            var name = "Aba";
-            Console.WriteLine($"My name is {name}");
+//            var name = "Aba";
+//            Console.WriteLine($"My name is {name}");
 
-            var number = "0123456789";
-            Console.WriteLine($"My number is {number}");
+//            var number = "0123456789";
+//            Console.WriteLine($"My number is {number}");
 
 
-            //Exercise - https://youtu.be/GlGpSQsS8uc?si=2LV6AJnwY7kWElap&t=2707
+//            //Exercise - https://youtu.be/GlGpSQsS8uc?si=2LV6AJnwY7kWElap&t=2707
 
-            /* Create & initalise two ints
-                *Make a variable to work out the remainder
-                *Output remainder to the screen
-                *Change the first int variable to another number
-                * and recalculate the remainder
-                * output new remainder to the screen
-                */
+//            /* Create & initalise two ints
+//                *Make a variable to work out the remainder
+//                *Output remainder to the screen
+//                *Change the first int variable to another number
+//                * and recalculate the remainder
+//                * output new remainder to the screen
+//                */
 
-            int first = 29049804;
-            int second = 43;
-            int remainder = first % second;
+//            int first = 29049804;
+//            int second = 43;
+//            int remainder = first % second;
 
-            Console.WriteLine(remainder);
+//            Console.WriteLine(remainder);
 
-            first = 31;
-            remainder= first % second;
+//            first = 31;
+//            remainder= first % second;
 
-            Console.WriteLine(remainder);
+//            Console.WriteLine(remainder);
 
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.ReadLine();
+//        }
+//    }
+//}
