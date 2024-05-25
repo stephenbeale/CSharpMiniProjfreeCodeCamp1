c435137aac7784034f7b3326068e5140dda4e6d0
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-25 11:15:34 +0100

    Do-while, too

--- Program_for_loops.cs
+++ Program_for_loops.cs
@@ -1,42 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Runtime.Remoting.Services;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Diagnostics.Eventing.Reader;
+//using System.Linq;
+//using System.Runtime.Remoting.Services;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml.Schema;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=59z9daoQH2qsHdS-&t=4462
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=59z9daoQH2qsHdS-&t=4462
 
-            Console.Write("How many times do you want to say hi?: ");
-            int loopCounter = Convert.ToInt32(Console.ReadLine());
+//            Console.Write("How many times do you want to say hi?: ");
+//            int loopCounter = Convert.ToInt32(Console.ReadLine());
 
-            if (loopCounter > 0)
-            {
-                for (int i = 0; i <= loopCounter; i++)
-                {
-                    Console.WriteLine($"i is {i}");
-                }
+//            if (loopCounter > 0)
+//            {
+//                for (int i = 0; i <= loopCounter; i++)
+//                {
+//                    Console.WriteLine($"i is {i}");
+//                }
 
-                for (int i = 0; i <= loopCounter; i += 2)
-                {
-                    Console.WriteLine($"i is {i}");
-                }
-            }
-            else 
-            {
-                Console.WriteLine("Can't start loop with 0");
-            };
+//                for (int i = 0; i <= loopCounter; i += 2)
+//                {
+//                    Console.WriteLine($"i is {i}");
+//                }
+//            }
+//            else 
+//            {
+//                Console.WriteLine("Can't start loop with 0");
+//            };
 
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.ReadLine();
+//        }
+//    }
+//}
---
+++ Program_while_loop.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=va5HeUwvQu_27L2P&t=5423
+
+            var i = 2;
+            var myNum = 30;
+
+            Console.WriteLine("While loop");
+            while (i != myNum)
+            {
+                Console.WriteLine($"i is {i}");
+                i++;
+            }
+
+            Console.WriteLine("Do-while");
+
+            i = 2;
+            myNum = 30;
+
+            do
+            {
+                //Condition is irrelevant first time
+                Console.WriteLine($"i is {i}");
+                i++;
+            } while (i != myNum);   
+
+            Console.ReadLine();
+        }
+    }
+}
