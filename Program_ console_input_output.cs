e7a72885851fe96ccf84d44b6474c79601b56173
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-25 10:46:47 +0100

    Switch days of the week

--- Program_ console_input_output.cs
+++ Program_ console_input_output.cs
@@ -1,72 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Runtime.Remoting.Services;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics.Eventing.Reader;
+//using System.Linq;
+//using System.Runtime.Remoting.Services;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml.Schema;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=7QJQU2NYWoqyxsM7&t=3862
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=7QJQU2NYWoqyxsM7&t=3862
 
-            //Console.Write("Enter your name:");
-            //string name = Console.ReadLine();
-            //Console.WriteLine($"Inputted name: {name}");
+//            //Console.Write("Enter your name:");
+//            //string name = Console.ReadLine();
+//            //Console.WriteLine($"Inputted name: {name}");
 
-            //Console.Write("Enter your age:");
-            ////Converts directly from input
-            //int agey = Convert.ToInt32(Console.ReadLine());
+//            //Console.Write("Enter your age:");
+//            ////Converts directly from input
+//            //int agey = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine($"Your age is {agey}");
+//            //Console.WriteLine($"Your age is {agey}");
 
-            //if (agey >= 18 && agey < 25)
-            //{
-            //    Console.WriteLine("You are allowed in because you are 18 or older");
-            //}
-            //else if(agey >= 25 && agey < 40)
-            //{
-            //    Console.WriteLine("25 or older");
-            //}
-            //else if (agey >= 40 && agey <= 75)
-            //{
-            //    Console.WriteLine("40 - 75 years");
-            //}
-            //if(agey < 0 || agey > 115)
-            //{
-            //    Console.WriteLine("Invalid age");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Valid age");
-            //};
+//            //if (agey >= 18 && agey < 25)
+//            //{
+//            //    Console.WriteLine("You are allowed in because you are 18 or older");
+//            //}
+//            //else if(agey >= 25 && agey < 40)
+//            //{
+//            //    Console.WriteLine("25 or older");
+//            //}
+//            //else if (agey >= 40 && agey <= 75)
+//            //{
+//            //    Console.WriteLine("40 - 75 years");
+//            //}
+//            //if(agey < 0 || agey > 115)
+//            //{
+//            //    Console.WriteLine("Invalid age");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("Valid age");
+//            //};
 
-            Console.Write("Enter first number: ");
-            int numberA = Convert.ToInt32(Console.ReadLine());
+//            Console.Write("Enter first number: ");
+//            int numberA = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter second number: ");
-            int numberB = Convert.ToInt32(Console.ReadLine());
+//            Console.Write("Enter second number: ");
+//            int numberB = Convert.ToInt32(Console.ReadLine());
 
-            int answer = numberA * numberB;
-            Console.Write($"What is {numberA} x {numberB}:");
-            string answerInput = Console.ReadLine();
-            int actualAsnwer = Convert.ToInt32(answerInput);
+//            int answer = numberA * numberB;
+//            Console.Write($"What is {numberA} x {numberB}:");
+//            string answerInput = Console.ReadLine();
+//            int actualAsnwer = Convert.ToInt32(answerInput);
 
-            if(answer != actualAsnwer)
-            {
-                Console.WriteLine("Sorry, try again");
-            }
-            else
-            {
-                Console.WriteLine("Well done!");
-            }
+//            if(answer != actualAsnwer)
+//            {
+//                Console.WriteLine("Sorry, try again");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Well done!");
+//            }
 
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.ReadLine();
+//        }
+//    }
+//}
---
+++ Program_ switch_statements.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=9wK6dyGZc7ZPgaKL&t=4126
+
+            Console.Write("Enter a day of the week as a number (1 = Monday, 7 = Sunday):");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1: 
+                    Console.WriteLine($"Day {day} is Monday");
+                    break;
+                case 2:
+                    Console.WriteLine($"Day {day} is Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine($"Day {day} is Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine($"Day {day} is Thursday");
+                    break;
+                case 5:
+                Console.WriteLine($"Day {day} is Wednesday");
+                    break;
+                case 6:
+                    Console.WriteLine($"Day {day} is Wednesday");
+                    break;
+                case 7:
+                    Console.WriteLine($"Day {day} is Wednesday");
+                    break;
+                default: 
+                    Console.WriteLine("Not a day of the week!");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
