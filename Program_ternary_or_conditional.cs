d27369ca89c25c34724584e5ad3c0c4837e27ec8
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-25 11:27:28 +0100

    Ternary or conditional operator

---
+++ Program_ternary_or_conditional.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=bOGNZKOvxPieRiw4&t=5566
+
+            //Too long
+            //if(age >= 0)
+            //{
+            //    Console.WriteLine("Valid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid");
+            //}
+
+            Console.WriteLine("How old can a person be?");
+            int ageInput = Convert.ToInt32(Console.ReadLine());
+
+            //Assign the variable as a result of the true false check on the condition
+            //condition ? true : false
+            string result = ageInput >= 0 ? "well done" : "idiot";
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
--- Program_while_loop.cs
+++ Program_while_loop.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Runtime.Remoting.Services;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Diagnostics.Eventing.Reader;
+//using System.Linq;
+//using System.Runtime.Remoting.Services;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml.Schema;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=va5HeUwvQu_27L2P&t=5423
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=va5HeUwvQu_27L2P&t=5423
 
-            var i = 2;
-            var myNum = 30;
+//            var i = 2;
+//            var myNum = 30;
 
-            Console.WriteLine("While loop");
-            while (i != myNum)
-            {
-                Console.WriteLine($"i is {i}");
-                i++;
-            }
+//            Console.WriteLine("While loop");
+//            while (i != myNum)
+//            {
+//                Console.WriteLine($"i is {i}");
+//                i++;
+//            }
 
-            Console.WriteLine("Do-while");
+//            Console.WriteLine("Do-while");
 
-            i = 2;
-            myNum = 30;
+//            i = 2;
+//            myNum = 30;
 
-            do
-            {
-                //Condition is irrelevant first time
-                Console.WriteLine($"i is {i}");
-                i++;
-            } while (i != myNum);   
+//            do
+//            {
+//                //Condition is irrelevant first time
+//                Console.WriteLine($"i is {i}");
+//                i++;
+//            } while (i != myNum);   
 
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.ReadLine();
+//        }
+//    }
+//}
