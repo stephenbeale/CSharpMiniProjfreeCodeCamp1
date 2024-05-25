a9f6d21bb80065fa5ba967a8f1cd460cd300095c
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-25 10:51:42 +0100

    for loops

---
+++ Program_for_loops.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=59z9daoQH2qsHdS-&t=4462
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i is {i}");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
