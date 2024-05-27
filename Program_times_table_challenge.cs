d2e353ce7561376ccb1cc59a6147e97e7b873108
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-27 17:01:55 +0100

    Outputs multiplication

---
+++ Program_times_table_challenge.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=2N-U96UooX97cxsT&t=7184
+
+            bool numInput = false;
+            Console.WriteLine("Enter a number to multiply by:");
+            var input = Console.ReadLine();
+
+            try
+            {
+                if (int.TryParse(input, out int result))
+                {
+                    Console.WriteLine($"Result is {result *= result}");
+                    
+                }
+            }
+            catch 
+            {
+                throw new Exception();
+
+            }          
+
+            Console.ReadLine();
+        }
+    }
+}
--- Program_try_parse.cs
+++ Program_try_parse.cs
@@ -1,42 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Diagnostics.Eventing.Reader;
-using System.Globalization;
-using System.Linq;
-using System.Runtime.Remoting.Services;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Diagnostics;
+//using System.Diagnostics.Eventing.Reader;
+//using System.Globalization;
+//using System.Linq;
+//using System.Runtime.Remoting.Services;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml.Schema;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=QMidGYoWBaMcy33O&t=5764
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=QMidGYoWBaMcy33O&t=5764
 
-            bool success = true;
+//            bool success = true;
 
-            while (success)
-            {
-                Console.Write("Enter a number: ");
-                string numInput = Console.ReadLine();
+//            while (success)
+//            {
+//                Console.Write("Enter a number: ");
+//                string numInput = Console.ReadLine();
 
-                if(int.TryParse(numInput, out int num))
-                {
-                    success = false;
-                    Console.WriteLine(num);
-                }
-                else
-                {
-                    Console.WriteLine("Failed to convert!");
-                }
-            }
+//                if(int.TryParse(numInput, out int num))
+//                {
+//                    success = false;
+//                    Console.WriteLine(num);
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Failed to convert!");
+//                }
+//            }
             
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.ReadLine();
+//        }
+//    }
+//}
