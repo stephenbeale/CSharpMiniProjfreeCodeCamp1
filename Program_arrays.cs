878f50243bd27bf5d73d17fd325b1ef950e6cb12
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-27 22:17:48 +0100

    Arrays at 3:12

---
+++ Program_arrays.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Runtime.Remoting.Services;
+using System.Threading;
+using System.Xml;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=C4YBjD936eo3vSKL&t=11568
+
+            Console.ReadLine();
+        }
+    }
+}
--- Program_string_functions.cs
+++ Program_string_functions.cs
@@ -1,92 +1,92 @@
-﻿using System;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Net.Http;
-using System.Runtime.Remoting.Services;
-using System.Threading;
-using System.Xml;
+﻿//using System;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using System.Net.Http;
+//using System.Runtime.Remoting.Services;
+//using System.Threading;
+//using System.Xml;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=5DKkVmujSlfY6pfA&t=9150
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=5DKkVmujSlfY6pfA&t=9150
 
-            Console.WriteLine("Enter your name: ");
-            string name = Console.ReadLine();
+//            Console.WriteLine("Enter your name: ");
+//            string name = Console.ReadLine();
 
-            if (name != string.Empty)
-            {
-                Console.WriteLine($"Your name is {name}");
-            }
-            else
-            {
-                Console.WriteLine("Name is empty.");
-            }
+//            if (name != string.Empty)
+//            {
+//                Console.WriteLine($"Your name is {name}");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Name is empty.");
+//            }
 
-            if (name != "Steve")
-            {
-                Console.WriteLine("You are not Steve");
-            }
+//            if (name != "Steve")
+//            {
+//                Console.WriteLine("You are not Steve");
+//            }
 
-            string message = "Hello";
+//            string message = "Hello";
 
-            char[] chars = new char[] { 'H', 'e', 'l', 'l', 'o' };
-            object newCompare = new string(chars);
+//            char[] chars = new char[] { 'H', 'e', 'l', 'l', 'o' };
+//            object newCompare = new string(chars);
 
-            if (message.Equals(newCompare))
-            {
-                Console.WriteLine("Same");
-            }
-            else
-            {
-                Console.WriteLine("Different");
-            };
+//            if (message.Equals(newCompare))
+//            {
+//                Console.WriteLine("Same");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Different");
+//            };
 
-            message = "C# is awesome\n";
+//            message = "C# is awesome\n";
 
-            for (int i = 0; i < message.Length; i++)
-            {
-                Console.Write(message[i]);
-                Thread.Sleep(50);
-            }
+//            for (int i = 0; i < message.Length; i++)
+//            {
+//                Console.Write(message[i]);
+//                Thread.Sleep(50);
+//            }
 
-            bool contains = false;
+//            bool contains = false;
 
-            for (int i = 0; i < message.Length; i++)
-            {
-                if (message[i] == '#')
-                {
-                    contains = true;
-                }
-                if (contains)
-                {
-                    Console.WriteLine("# found in your string");
-                    break;
-                }
-            }
+//            for (int i = 0; i < message.Length; i++)
+//            {
+//                if (message[i] == '#')
+//                {
+//                    contains = true;
+//                }
+//                if (contains)
+//                {
+//                    Console.WriteLine("# found in your string");
+//                    break;
+//                }
+//            }
 
-            //Belt and braces - if null, do not get exception for this.
-            if(!string.IsNullOrEmpty(name)) {
-                Console.WriteLine("String is not null or empty!");
-            }
+//            //Belt and braces - if null, do not get exception for this.
+//            if(!string.IsNullOrEmpty(name)) {
+//                Console.WriteLine("String is not null or empty!");
+//            }
 
-            Console.WriteLine(message.Contains("#"));
+//            Console.WriteLine(message.Contains("#"));
 
-            //Print string in reverse challenge
+//            //Print string in reverse challenge
 
-            char[] messageChars = message.ToCharArray();
-            for (int i = messageChars.Length; i > 0; i--)
-            {
-                if (!string.IsNullOrEmpty(message))
-                {
-                    Console.Write(messageChars[i - 1]);
-                }
-            }
+//            char[] messageChars = message.ToCharArray();
+//            for (int i = messageChars.Length; i > 0; i--)
+//            {
+//                if (!string.IsNullOrEmpty(message))
+//                {
+//                    Console.Write(messageChars[i - 1]);
+//                }
+//            }
 
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.ReadLine();
+//        }
+//    }
+//}
