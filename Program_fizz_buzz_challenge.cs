a749dfe356e639a9fc0834cec6cabc332e0e947d
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-27 17:46:52 +0100

    Escape characters

--- Program_fizz_buzz_challenge.cs
+++ Program_fizz_buzz_challenge.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Threading;
+﻿//using System;
+//using System.Threading;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=QaMgoRyJzC5FooBY&t=7388
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=QaMgoRyJzC5FooBY&t=7388
             
-                for (int i = 1; i <= 15; i++)
-                {
-                bool threeDiv = i % 3 == 0;
-                bool fiveDiv  = i % 5 == 0;
-                if ((threeDiv && fiveDiv)) 
-                    {
-                    Console.WriteLine("Fizz Buzz");                                        
-                    }
-                    if(fiveDiv)
-                    {
-                    Console.WriteLine("Buzz");
-                    }
-                    else if (threeDiv)
-                    {
-                    Console.WriteLine("Fizz");
-                    }
-                    else 
-                    {
-                    Console.WriteLine(i);
-                    };
-                }
-            Console.ReadLine();
-        }
-    }
-}
+//                for (int i = 1; i <= 15; i++)
+//                {
+//                bool threeDiv = i % 3 == 0;
+//                bool fiveDiv  = i % 5 == 0;
+//                if ((threeDiv && fiveDiv)) 
+//                    {
+//                    Console.WriteLine("Fizz Buzz");                                        
+//                    }
+//                    if(fiveDiv)
+//                    {
+//                    Console.WriteLine("Buzz");
+//                    }
+//                    else if (threeDiv)
+//                    {
+//                    Console.WriteLine("Fizz");
+//                    }
+//                    else 
+//                    {
+//                    Console.WriteLine(i);
+//                    };
+//                }
+//            Console.ReadLine();
+//        }
+//    }
+//}
---
+++ Program_verbatim_string_literal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=Uh0VVAKusAsQHjFV&t=8162
+
+            //\t \n \" \\ = \
+
+            
+        }
+    }
+}
