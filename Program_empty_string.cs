1672cdeb8d178f28992dfdf0ba239ee4ecc7e73d
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-27 18:25:21 +0100

    string . empty example

---
+++ Program_empty_string.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Remoting.Services;
+using System.Threading;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=5DKkVmujSlfY6pfA&t=9150
+
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            
+            if(name != string.Empty)
+            {
+                Console.WriteLine($"Your name is {name}");
+            }
+            else
+            {
+                Console.WriteLine("Name is empty.");
+            }
+            Console.ReadLine();            
+        }
+    }
+}
--- Program_string_formatting.cs
+++ Program_string_formatting.cs
@@ -1,30 +1,30 @@
-﻿using System;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading;
+﻿//using System;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using System.Threading;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=I0MIHaL5uByhyo0W&t=8211
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=I0MIHaL5uByhyo0W&t=8211
 
-            var string1 = "first";
-            var string2 = "second";
-            var string3 = "third";
+//            var string1 = "first";
+//            var string2 = "second";
+//            var string3 = "third";
 
-            string[] names = { string1, string2, string3 };
-            string[] names2 = { "me", "you", "him" };
+//            string[] names = { string1, string2, string3 };
+//            string[] names2 = { "me", "you", "him" };
 
-            names.ToList().ForEach(x => Console.WriteLine($"Names now: {x}"));
-            Console.WriteLine(names2.Length);
-            names2.CopyTo(names, 0);
-            //Using Linq
-            names.ToList().ForEach(x => Console.WriteLine($"Names now: {x}"));
+//            names.ToList().ForEach(x => Console.WriteLine($"Names now: {x}"));
+//            Console.WriteLine(names2.Length);
+//            names2.CopyTo(names, 0);
+//            //Using Linq
+//            names.ToList().ForEach(x => Console.WriteLine($"Names now: {x}"));
 
-            Console.ReadLine();            
-        }
-    }
-}
+//            Console.ReadLine();            
+//        }
+//    }
+//}
