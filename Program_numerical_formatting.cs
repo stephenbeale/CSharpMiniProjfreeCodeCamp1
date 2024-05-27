8c1be2555c0b19afe0265d7e6d6ede78e5de7c8c
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-27 15:49:09 +0100

    formatting to 2 decimal places

---
+++ Program_numerical_formatting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=QMidGYoWBaMcy33O&t=5764
+
+            double value = 1000D / 12.34D;
+
+            Console.WriteLine(value);
+            //Pattern to format to
+            Console.WriteLine(string.Format("{0}, {1}, {2}", value, 1000, 8));
+           
+            Console.ReadLine();
+        }
+    }
+}
