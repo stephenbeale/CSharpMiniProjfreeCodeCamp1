a71fa69d78c9cc139e7344eb694866b0a5c04bd9
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-27 17:48:53 +0100

    Escape chars test

--- Program_verbatim_string_literal.cs
+++ Program_verbatim_string_literal.cs
@@ -10,6 +10,14 @@ namespace CSharpMiniProjfreeCodeCamp1
             // https://youtu.be/GlGpSQsS8uc?si=Uh0VVAKusAsQHjFV&t=8162
 
             //\t \n \" \\ = \
+            var speech = "He said \"something\"";
+            var path = "C:\\Users\\CoffeeNCode\\Desktop\\C# Course\nNew line test";
+
+            Console.WriteLine(path);
+            Console.WriteLine(speech);
+
+            Console.ReadLine();
+        
 
             
         }
