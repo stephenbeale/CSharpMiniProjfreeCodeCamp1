eb0c14b745c7c90297603e91d03f197e623d82d1
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-27 17:42:51 +0100

    Naive implementation of fizz buzz

---
+++ Program_fizz_buzz_challenge.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=QaMgoRyJzC5FooBY&t=7388
+            
+                for (int i = 1; i <= 15; i++)
+                {
+                    if((i % 3 == 0 && i % 5 == 0)) 
+                    {
+                    Console.WriteLine("Fizz Buzz");                                        
+                    }
+                    if(i % 5 == 0)
+                    {
+                    Console.WriteLine("Buzz");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                    Console.WriteLine("Fizz");
+                    }
+                    else 
+                    {
+                    Console.WriteLine(i);
+                    };
+                }
+            Console.ReadLine();
+        }
+    }
+}
--- Program_times_table_challenge.cs
+++ Program_times_table_challenge.cs
@@ -1,36 +1,36 @@
-﻿using System;
+﻿//using System;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=2N-U96UooX97cxsT&t=7184
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=2N-U96UooX97cxsT&t=7184
 
-            bool numInput = false;
-            Console.WriteLine("Enter a number to multiply by:");
-            var input = Console.ReadLine();
+//            bool numInput = false;
+//            Console.WriteLine("Enter a number to multiply by:");
+//            var input = Console.ReadLine();
             
-                if (int.TryParse(input, out int result))
-                {
-                    Console.WriteLine("Times table is:");
+//                if (int.TryParse(input, out int result))
+//                {
+//                    Console.WriteLine("Times table is:");
 
-                    for (int i = 1; i <= 12; i++)
-                    {
-                        //My way
-                        Console.WriteLine($"My way: {i}: {i} * {result} = {i * result}");
+//                    for (int i = 1; i <= 12; i++)
+//                    {
+//                        //My way
+//                        Console.WriteLine($"My way: {i}: {i} * {result} = {i * result}");
 
-                    //Course way
-                    //First arg corresponds to params passed as arguments
-                    Console.WriteLine("Course way: {0} x {1} = {2}", i, input, i * result);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input");
-                }
-            Console.ReadLine();
-        }
-    }
-}
+//                    //Course way
+//                    //First arg corresponds to params passed as arguments
+//                    Console.WriteLine("Course way: {0} x {1} = {2}", i, input, i * result);
+//                    }
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Invalid input");
+//                }
+//            Console.ReadLine();
+//        }
+//    }
+//}
