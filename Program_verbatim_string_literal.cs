2bf38d3d9384bae63ad88c033ba37c5380c4b4cf
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-27 18:10:45 +0100

    Copying arrays, my stuff

--- Program_verbatim_string_literal.cs
+++ Program_verbatim_string_literal.cs
@@ -1,34 +1,34 @@
-﻿using System;
-using System.Linq.Expressions;
-using System.Threading;
+﻿//using System;
+//using System.Linq.Expressions;
+//using System.Threading;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=Uh0VVAKusAsQHjFV&t=8162
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=Uh0VVAKusAsQHjFV&t=8162
 
-            //\t \n \" \\ = \
-            Console.WriteLine("String of speech and path:");
-            var speech = "He said \"something\"";
-            var path = "C:\\Users\\CoffeeNCode\\Desktop\\C# Course\nNew line test";
+//            //\t \n \" \\ = \
+//            Console.WriteLine("String of speech and path:");
+//            var speech = "He said \"something\"";
+//            var path = "C:\\Users\\CoffeeNCode\\Desktop\\C# Course\nNew line test";
 
-            Console.WriteLine(path);
-            Console.WriteLine(speech);
+//            Console.WriteLine(path);
+//            Console.WriteLine(speech);
 
-            //SO - 'It marks the string as a verbatim string literal - anything in the string that would normally be interpreted as an escape sequence is ignored.'
+//            //SO - 'It marks the string as a verbatim string literal - anything in the string that would normally be interpreted as an escape sequence is ignored.'
 
-            path = @"C:\Users\CoffeeNCode\Desktop\C# Course\nNew line test";
-            Console.WriteLine($"Verbatim string literal: {path}");
+//            path = @"C:\Users\CoffeeNCode\Desktop\C# Course\nNew line test";
+//            Console.WriteLine($"Verbatim string literal: {path}");
 
-            var name = @"Hello ""someone""";
-            Console.WriteLine();
+//            var name = @"Hello ""someone""";
+//            Console.WriteLine();
 
-            name = "Hello 'someone'";
-            Console.WriteLine(name);
-            Console.ReadLine();            
-        }
-    }
-}
+//            name = "Hello 'someone'";
+//            Console.WriteLine(name);
+//            Console.ReadLine();            
+//        }
+//    }
+//}
