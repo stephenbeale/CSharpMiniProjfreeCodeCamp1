84452f52fdd7714dabe4db4caaf30237d1d4cab4
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-28 21:12:31 +0100

    Interesting properties and ctors

---
+++ Program_properties.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Services;
+using System.Threading;
+using System.Xml;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=rdy9HinTWts0TncC&t=27848
+
+            //Person person = new Person("Mr", 1, "Steve");
+            //Person person = new Person();
+            //Can assign here without constructor
+            //person.Id = 1;
+            //person.Name = "Rich";
+            //person.Title = "Test";
+
+            Person person1= new Person(2, "Stu", "Squire");
+
+
+
+            Console.WriteLine(person1.Name);
+            //Console.WriteLine(personWithoutConstructor);          
+            //Console.WriteLine(personWithoutConstructor.ToString());          
+            Console.WriteLine(person1.Id);          
+            Console.WriteLine(person1.Title);          
+            //Console.WriteLine(personWithoutConstructor.GetType());          
+
+            Console.ReadLine();
+        }
+
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Title { get; set; }
+
+            public Person(int id, string name, string title)
+            {
+                Id= id;
+                Name= name;
+                Title= title;
+            }
+        }
+
+    }
+}
--- Program_structs.cs
+++ Program_structs.cs
@@ -1,58 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Net.Http;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Services;
-using System.Threading;
-using System.Xml;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using System.Net.Http;
+//using System.Reflection;
+//using System.Runtime.InteropServices;
+//using System.Runtime.Remoting.Services;
+//using System.Threading;
+//using System.Xml;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=i9d92AhIv49AiKzJ&t=24635      
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=i9d92AhIv49AiKzJ&t=24635      
 
-            //Person person = new Person("Mr", 1, "Steve");
-            Person personWithoutConstructor;
+//            //Person person = new Person("Mr", 1, "Steve");
+//            Person personWithoutConstructor;
 
-            personWithoutConstructor.Id = 1;
-            personWithoutConstructor.Name = "John";
-            personWithoutConstructor.Title = "Captain";
+//            personWithoutConstructor.Id = 1;
+//            personWithoutConstructor.Name = "John";
+//            personWithoutConstructor.Title = "Captain";
 
-            Console.WriteLine(personWithoutConstructor.Name);
-            //Console.WriteLine(personWithoutConstructor);          
-            //Console.WriteLine(personWithoutConstructor.ToString());          
-            Console.WriteLine(personWithoutConstructor.Id);          
-            Console.WriteLine(personWithoutConstructor.Title);          
-            //Console.WriteLine(personWithoutConstructor.GetType());          
+//            Console.WriteLine(personWithoutConstructor.Name);
+//            //Console.WriteLine(personWithoutConstructor);          
+//            //Console.WriteLine(personWithoutConstructor.ToString());          
+//            Console.WriteLine(personWithoutConstructor.Id);          
+//            Console.WriteLine(personWithoutConstructor.Title);          
+//            //Console.WriteLine(personWithoutConstructor.GetType());          
 
-            Console.ReadLine();
-        }
+//            Console.ReadLine();
+//        }
 
-        public struct Person
-        {
-            public string Name;
-            public int Id;
-            public string Title;
+//        public struct Person
+//        {
+//            public string Name;
+//            public int Id;
+//            public string Title;
 
-            //Can use a constructor
-            //public Person(string title, int id, string name)
-            //{
-            //    Title= title;
-            //    Id= id;
-            //    Name= name;
-            //}
-        }
+//            //Can use a constructor
+//            //public Person(string title, int id, string name)
+//            //{
+//            //    Title= title;
+//            //    Id= id;
+//            //    Name= name;
+//            //}
+//        }
 
-        public class Persona
-        {
+//        public class Persona
+//        {
 
-        }
+//        }
 
-    }
-}
+//    }
+//}
