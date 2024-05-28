cf432d0ddaf63b9ca2b26c741cc9d0c8f1dfad50
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-28 20:53:07 +0100

    struct with constructor

--- Program_exception_handling.cs
+++ Program_exception_handling.cs
@@ -1,52 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Net.Http;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Services;
-using System.Threading;
-using System.Xml;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using System.Net.Http;
+//using System.Reflection;
+//using System.Runtime.InteropServices;
+//using System.Runtime.Remoting.Services;
+//using System.Threading;
+//using System.Xml;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=P0siFmWiXEtHdPDM&t=21652
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=P0siFmWiXEtHdPDM&t=21652
 
-            bool looping = false;
+//            bool looping = false;
 
-            while (!looping)
-            {
-                try
-                {
-                    Console.WriteLine("Enter a number: ");
-                    int num = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine($"Your number: {num}");
+//            while (!looping)
+//            {
+//                try
+//                {
+//                    Console.WriteLine("Enter a number: ");
+//                    int num = Convert.ToInt32(Console.ReadLine());
+//                    Console.WriteLine($"Your number: {num}");
 
-                    looping = true;
-                }
-                //catch (FormatException)
-                //{
-                //    Console.WriteLine("Please only enter numbers");
+//                    looping = true;
+//                }
+//                //catch (FormatException)
+//                //{
+//                //    Console.WriteLine("Please only enter numbers");
 
-                //}
-                //catch (OverflowException)
-                //{
-                //    Console.WriteLine("Please enter a number less than 2 billion");
+//                //}
+//                //catch (OverflowException)
+//                //{
+//                //    Console.WriteLine("Please enter a number less than 2 billion");
 
-                //}
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Error: {e.Message}");
-                    Console.WriteLine("Exception!");
-                }
-            }
+//                //}
+//                catch (Exception e)
+//                {
+//                    Console.WriteLine($"Error: {e.Message}");
+//                    Console.WriteLine("Exception!");
+//                }
+//            }
 
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.ReadLine();
+//        }
+//    }
+//}
---
+++ Program_structs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Services;
+using System.Threading;
+using System.Xml;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=i9d92AhIv49AiKzJ&t=24635      
+
+            Person person = new Person("Mr", 1, "Steve");
+            Console.WriteLine(person.Name);          
+
+            Console.ReadLine();
+        }
+
+        public struct Person
+        {
+            public string Name;
+            public int Id;
+            public string Title;
+
+            public Person(string title, int id, string name)
+            {
+                Title= title;
+                Id= id;
+                Name= name;
+            }
+        }
+    }
+}
