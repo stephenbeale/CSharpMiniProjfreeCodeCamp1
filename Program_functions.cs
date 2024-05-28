bca233eaa279732a41d83fcbe6707e13af00637c
Steve Beale <s.j.beale@swansea.ac.uk>
2024-05-28 15:20:20 +0100

    Change order of functions passed by specifying the name in the call

---
+++ Program_functions.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Services;
+using System.Threading;
+using System.Xml;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=MbhQecTF7SMUpdSF&t=19092
+
+            //Can call functions directly 
+            Console.WriteLine($"{HelloWorld2()}");
+
+            //Console.WriteLine($"{helloWorldy()}");
+            Console.WriteLine($"Multiplier {Multiplier(4)}");
+            Console.WriteLine($"{Add(3)}");
+
+            Console.WriteLine($"Lambda anonymous: {addLambdaAnon(2, 3)}"); // Output: 5
+
+            Console.WriteLine($"Anonymous function {stringAnonFunc("Steve")}");
+
+            var name = "Steve";
+            var age = 40;
+            var address = "1 Road Street";
+
+            PrintDetails( name, age, address );
+
+            //Specify and can change the order
+            PrintDetails(agey: age, addressy : address, namey : name);
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+
+        static void PrintDetails(string namey, int agey, string addressy)
+        {
+            Console.WriteLine($"Name: {namey}");
+            Console.WriteLine($"Age: {agey}");
+            Console.WriteLine($"Address: {addressy}");
+        }
+
+        static void HelloWorldy()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static string HelloWorld2()
+        {
+            return "Hello World!";
+        }
+
+        static int Multiplier(int num)
+        {
+            return num * num;
+        }
+
+        static int Add(int num)
+        {
+            return num + num;
+        }
+
+        //Optional params with that keyword
+        //Default a better option for optional
+        static int Add(int num, int num2 = default)
+        {
+            return num + num;
+        }
+
+        //Inline function initialisation
+        static int Divider(int numerator, int denominator) => numerator / denominator;
+
+        //Single-line property getter
+        public int MyProperty { get; } = 42;
+        public int DoubleProperty => MyProperty * 2;
+
+        //Single-line Lambda function - anonymous or lambda function
+        // <in1, in2, out result> name = parameters => action on parameters
+        static Func<int, int, int> addLambdaAnon = (x, y) => x + y;
+
+        static Func<string, string> stringAnonFunc = (name) => name.Insert(0, "AnonymousFunctionCalled ");
+    }
+}
--- Program_odd-even-number-split.cs
+++ Program_odd-even-number-split.cs
@@ -1,114 +1,114 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Net.Http;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Services;
-using System.Threading;
-using System.Xml;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using System.Net.Http;
+//using System.Reflection;
+//using System.Runtime.InteropServices;
+//using System.Runtime.Remoting.Services;
+//using System.Threading;
+//using System.Xml;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=zSPosrptEZipT6M2&t=15371
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=zSPosrptEZipT6M2&t=15371
 
-            List<int> odds = new List<int>();
-            List<int> evens = new List<int>();
+//            List<int> odds = new List<int>();
+//            List<int> evens = new List<int>();
 
-            for (int i = 0; i < 20; i++) 
-            {
-                if (i % 2 != 0)
-                {
-                    odds.Add(i);
-                }
-                else 
-                { 
-                    evens.Add(i);
-                }
-            }
+//            for (int i = 0; i < 20; i++) 
+//            {
+//                if (i % 2 != 0)
+//                {
+//                    odds.Add(i);
+//                }
+//                else 
+//                { 
+//                    evens.Add(i);
+//                }
+//            }
 
-            Console.WriteLine($"Odds list:");
-            foreach (int i in odds)
-            {
-                Console.WriteLine($"{i}");
-            }
+//            Console.WriteLine($"Odds list:");
+//            foreach (int i in odds)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
             
-            Console.WriteLine($"Evens list: {evens}");
-            foreach (int i in evens)
-            {
-                Console.WriteLine($"{i}");
-            }
+//            Console.WriteLine($"Evens list: {evens}");
+//            foreach (int i in evens)
+//            {
+//                Console.WriteLine($"{i}");
+//            }
 
 
-            //Array insert task - https://youtu.be/GlGpSQsS8uc?si=aDLLEj33HNPkQJxy&t=15789
+//            //Array insert task - https://youtu.be/GlGpSQsS8uc?si=aDLLEj33HNPkQJxy&t=15789
 
-            Dictionary<int, int> numLength = new Dictionary<int, int>()
-            {
-                {1, 20 },
-                {2, 30},
-                {3, 60}
-            };
+//            Dictionary<int, int> numLength = new Dictionary<int, int>()
+//            {
+//                {1, 20 },
+//                {2, 30},
+//                {3, 60}
+//            };
 
-            HelloWorldy();
+//            HelloWorldy();
 
 
-            //Can call functions directly 
-            Console.WriteLine($"{HelloWorld2()}");
+//            //Can call functions directly 
+//            Console.WriteLine($"{HelloWorld2()}");
 
-            //Console.WriteLine($"{helloWorldy()}");
-            Console.WriteLine($"Multiplier {Multiplier(4)}");
-            Console.WriteLine($"{Add(3)}");
+//            //Console.WriteLine($"{helloWorldy()}");
+//            Console.WriteLine($"Multiplier {Multiplier(4)}");
+//            Console.WriteLine($"{Add(3)}");
 
-            Console.WriteLine($"Lambda anonymous: {addLambdaAnon(2, 3)}"); // Output: 5
+//            Console.WriteLine($"Lambda anonymous: {addLambdaAnon(2, 3)}"); // Output: 5
 
-            Console.WriteLine($"Anonymous function {stringAnonFunc("Steve")}");
+//            Console.WriteLine($"Anonymous function {stringAnonFunc("Steve")}");
 
-            Console.ReadLine();
-        }
+//            Console.ReadLine();
+//        }
 
-        static void HelloWorldy()
-        {
-            Console.WriteLine("Hello World!");
-        }
+//        static void HelloWorldy()
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
 
-        static string HelloWorld2()
-        {
-            return "Hello World!";
-        }
+//        static string HelloWorld2()
+//        {
+//            return "Hello World!";
+//        }
 
-        static int Multiplier(int num)
-        {
-            return num * num;
-        }
+//        static int Multiplier(int num)
+//        {
+//            return num * num;
+//        }
 
-        static int Add(int num)
-        {
-            return num + num;
-        }
+//        static int Add(int num)
+//        {
+//            return num + num;
+//        }
 
-        //Optional params with that keyword
-        //Default a better option for optional
-        static int Add(int num, int num2 = default)
-        {
-            return num + num;
-        }
+//        //Optional params with that keyword
+//        //Default a better option for optional
+//        static int Add(int num, int num2 = default)
+//        {
+//            return num + num;
+//        }
 
-        //Inline function initialisation
-        static int Divider(int numerator, int denominator) => numerator / denominator;
+//        //Inline function initialisation
+//        static int Divider(int numerator, int denominator) => numerator / denominator;
 
-        //Single-line property getter
-        public int MyProperty { get; } = 42;
-        public int DoubleProperty => MyProperty * 2;
+//        //Single-line property getter
+//        public int MyProperty { get; } = 42;
+//        public int DoubleProperty => MyProperty * 2;
 
-        //Single-line Lambda function - anonymous or lambda function
-        // <in1, in2, out result> name = parameters => action on parameters
-        static Func<int, int, int> addLambdaAnon = (x, y) => x + y;
+//        //Single-line Lambda function - anonymous or lambda function
+//        // <in1, in2, out result> name = parameters => action on parameters
+//        static Func<int, int, int> addLambdaAnon = (x, y) => x + y;
 
-        static Func<string, string> stringAnonFunc = (name) => name.Insert(0, "AnonymousFunctionCalled ");
-    }
-}
+//        static Func<string, string> stringAnonFunc = (name) => name.Insert(0, "AnonymousFunctionCalled ");
+//    }
+//}
