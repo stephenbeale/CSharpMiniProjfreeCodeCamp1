de472471f9bea14acec34001b8b92bd246f68783
Steve Beale <s.j.beale@swansea.ac.uk>
2024-05-28 13:56:48 +0100

    Messing with KV pairs

---
+++ Program_dictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.Remoting.Services;
+using System.Threading;
+using System.Xml;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=ZX5fYzZlXMvn8xYx&t=14620
+
+
+            //Dictionary of type int (key) and string (value)
+            Dictionary<int, string> names = new Dictionary<int, string>();
+
+            names.Add(1, "Steve");
+            names.Add(2, "Me");
+            names.Add(3, "You");
+            names.Add(4, "He");
+            names.Add(5, "She");
+
+            Dictionary<int, string> names2 = new Dictionary<int, string>
+            {
+                {1,"Steve"},
+                {2,"Me"},
+                {3,"You"},
+                {4,"He"},
+                {5,"She"}
+            };
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names.ElementAt(i));
+            }
+
+            Console.WriteLine($"Get specific element at position {4}: {names.ElementAt(4)}");
+
+            KeyValuePair<int, string> pair = names.ElementAt(2);
+            Console.WriteLine($"Pair key: {pair.Key}");
+            Console.WriteLine($"Pair value: {pair.Value}");
+            Console.WriteLine($"Pair: {pair.Key} : {pair.Value}");
+
+            foreach (KeyValuePair<int, string> item in names)
+            {
+                Console.WriteLine(pair);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- Program_lists.cs
+++ Program_lists.cs
@@ -1,48 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Net.Http;
-using System.Runtime.Remoting.Services;
-using System.Threading;
-using System.Xml;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using System.Net.Http;
+//using System.Runtime.Remoting.Services;
+//using System.Threading;
+//using System.Xml;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=iTlKfQQvk02s5fkX&t=14128
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=iTlKfQQvk02s5fkX&t=14128
 
-            List<string> list = new List<string>();
-            List<int> list2 = new List<int>()
-            {
-                1,23,264,5,465,8789
-            };
+//            List<string> list = new List<string>();
+//            List<int> list2 = new List<int>()
+//            {
+//                1,23,264,5,465,8789
+//            };
 
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine("Enter a number:");                
-                int num = Convert.ToInt32(Console.ReadLine());
-                list2.Add(num);
-            }
+//            for (int i = 0; i < 3; i++)
+//            {
+//                Console.WriteLine("Enter a number:");                
+//                int num = Convert.ToInt32(Console.ReadLine());
+//                list2.Add(num);
+//            }
 
-            for (int i = 0;i < list2.Count; i++)
-            {
-                Console.WriteLine(list2[i]);
-            }
+//            for (int i = 0;i < list2.Count; i++)
+//            {
+//                Console.WriteLine(list2[i]);
+//            }
 
-            list2.ForEach(x => Console.WriteLine(x));
+//            list2.ForEach(x => Console.WriteLine(x));
 
-            list2.RemoveAt(3);
+//            list2.RemoveAt(3);
 
-            foreach (int num in list2)
-            {
-                Console.WriteLine(num);
-            }
+//            foreach (int num in list2)
+//            {
+//                Console.WriteLine(num);
+//            }
 
-            Console.ReadLine();
-        }
-    }
-}
+//            Console.ReadLine();
+//        }
+//    }
+//}
