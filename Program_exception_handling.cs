43edce5743d601baf9d7d896cd0db2d2cad1c6b5
Stephen Beale <s.j.beale@swansea.ac.uk>
2024-05-28 20:00:40 +0100

    Exceptions added for variety

---
+++ Program_exception_handling.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Services;
+using System.Threading;
+using System.Xml;
+
+namespace CSharpMiniProjfreeCodeCamp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/GlGpSQsS8uc?si=P0siFmWiXEtHdPDM&t=21652
+
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Your number: {num}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please only enter numbers");
+                
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Please enter a number less than 2 billion");
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception!");
+            }
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
--- Program_functions.cs
+++ Program_functions.cs
@@ -1,94 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Net.Http;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Services;
-using System.Threading;
-using System.Xml;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using System.Net.Http;
+//using System.Reflection;
+//using System.Runtime.InteropServices;
+//using System.Runtime.Remoting.Services;
+//using System.Threading;
+//using System.Xml;
 
-namespace CSharpMiniProjfreeCodeCamp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // https://youtu.be/GlGpSQsS8uc?si=MbhQecTF7SMUpdSF&t=19092
+//namespace CSharpMiniProjfreeCodeCamp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // https://youtu.be/GlGpSQsS8uc?si=MbhQecTF7SMUpdSF&t=19092
 
-            //Can call functions directly 
-            Console.WriteLine($"{HelloWorld2()}");
+//            //Can call functions directly 
+//            Console.WriteLine($"{HelloWorld2()}");
 
-            //Console.WriteLine($"{helloWorldy()}");
-            Console.WriteLine($"Multiplier {Multiplier(4)}");
-            Console.WriteLine($"{Add(3)}");
+//            //Console.WriteLine($"{helloWorldy()}");
+//            Console.WriteLine($"Multiplier {Multiplier(4)}");
+//            Console.WriteLine($"{Add(3)}");
 
-            Console.WriteLine($"Lambda anonymous: {addLambdaAnon(2, 3)}"); // Output: 5
+//            Console.WriteLine($"Lambda anonymous: {addLambdaAnon(2, 3)}"); // Output: 5
 
-            Console.WriteLine($"Anonymous function {stringAnonFunc("Steve")}");
+//            Console.WriteLine($"Anonymous function {stringAnonFunc("Steve")}");
 
-            var name = "Steve";
-            var age = 40;
-            var address = "1 Road Street";
+//            var name = "Steve";
+//            var age = 40;
+//            var address = "1 Road Street";
 
-            PrintDetails( name, age, address );
+//            PrintDetails( name, age, address );
 
-            //Specify and can change the order
-            PrintDetails(agey: age, 
-                addressy : address, 
-                namey : name);
+//            //Specify and can change the order
+//            PrintDetails(agey: age, 
+//                addressy : address, 
+//                namey : name);
 
-            Console.WriteLine();
+//            Console.WriteLine();
 
-            Console.ReadLine();
-        }
+//            int num = 10;
 
-        static void PrintDetails(string namey, int agey, string addressy)
-        {
-            Console.WriteLine($"Name: {namey}");
-            Console.WriteLine($"Age: {agey}");
-            Console.WriteLine($"Address: {addressy}");
-        }
+//            Assign(ref num);
+            
+//            Console.ReadLine();
+//        }
 
-        static void HelloWorldy()
-        {
-            Console.WriteLine("Hello World!");
-        }
 
-        static string HelloWorld2()
-        {
-            return "Hello World!";
-        }
+//        //Reference parameters - pass parameters by reference, not value - ref keyword. A bit like a pointer
+//        static void Assign(ref int num)
+//        {
+//            num = 20;
+//        }
 
-        static int Multiplier(int num)
-        {
-            return num * num;
-        }
+//        static void PrintDetails(string namey, int agey, string addressy)
+//        {
+//            Console.WriteLine($"Name: {namey}");
+//            Console.WriteLine($"Age: {agey}");
+//            Console.WriteLine($"Address: {addressy}");
+//        }
 
-        static int Add(int num)
-        {
-            return num + num;
-        }
+//        static void HelloWorldy()
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
 
-        //Optional params with that keyword
-        //Default a better option for optional
-        static int Add(int num, int num2 = default)
-        {
-            return num + num;
-        }
+//        static string HelloWorld2()
+//        {
+//            return "Hello World!";
+//        }
 
-        //Inline function initialisation
-        static int Divider(int numerator, int denominator) => numerator / denominator;
+//        static int Multiplier(int num)
+//        {
+//            return num * num;
+//        }
 
-        //Single-line property getter
-        public int MyProperty { get; } = 42;
-        public int DoubleProperty => MyProperty * 2;
+//        static int Add(int num)
+//        {
+//            return num + num;
+//        }
 
-        //Single-line Lambda function - anonymous or lambda function
-        // <in1, in2, out result> name = parameters => action on parameters
-        static Func<int, int, int> addLambdaAnon = (x, y) => x + y;
+//        //Optional params with that keyword
+//        //Default a better option for optional
+//        static int Add(int num, int num2 = default)
+//        {
+//            return num + num;
+//        }
 
-        static Func<string, string> stringAnonFunc = (name) => name.Insert(0, "AnonymousFunctionCalled ");
-    }
-}
+//        //Inline function initialisation
+//        static int Divider(int numerator, int denominator) => numerator / denominator;
+
+//        //Single-line property getter
+//        public int MyProperty { get; } = 42;
+//        public int DoubleProperty => MyProperty * 2;
+
+//        //Single-line Lambda function - anonymous or lambda function
+//        // <in1, in2, out result> name = parameters => action on parameters
+//        static Func<int, int, int> addLambdaAnon = (x, y) => x + y;
+
+//        static Func<string, string> stringAnonFunc = (name) => name.Insert(0, "AnonymousFunctionCalled ");
+//    }
+//}
