e7a78fab0fdace1ab83ed9ce1c90b2677cedc706
Steve Beale <s.j.beale@swansea.ac.uk>
2024-05-28 11:56:36 +0100

    Adding numbers to a list

--- Program_lists.cs
+++ Program_lists.cs
@@ -16,7 +16,23 @@ namespace CSharpMiniProjfreeCodeCamp1
             // https://youtu.be/GlGpSQsS8uc?si=iTlKfQQvk02s5fkX&t=14128
 
             List<string> list = new List<string>();
-            
+            List<int> list2 = new List<int>()
+            {
+                1,23,264,5,465,8789
+            };
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter a number:");                
+                int num = Convert.ToInt32(Console.ReadLine());
+                list2.Add(num);
+            }
+
+            foreach (int num in list2)
+            {
+                Console.WriteLine(num);
+            }
+
             Console.ReadLine();
         }
     }
